f71f37cd64aa3010cb3d7570892babb9fed9626e
qsping <ws02011@126.com>
2024-04-11 21:14:18 +0800

    Create main.c

---
+++ C语言/3.10/main.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+//	struct stu
+//	{
+//		char name[20];
+//		int age;
+//		char sex[20];
+//
+//	};
+//	int main()
+//{
+//		struct stu s = { "ws",20,"male" };
+//		printf("%s %d %s",s.name,s.age,s.sex );
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d", a / b, a % b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a;
+//	if (a = 1)
+//	{
+//		printf("aa");
+//	}
+//
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int  a ;
+//	int b =0;
+//	while ( b <= 49)
+//	{
+//		a = 2 * b + 1;				
+//		printf("%d\n", a);
+//		b++;
+//	}
+//	return 0;		
+//}
+//int main()
+//{
+//	int a = 1;
+//	while (a <= 10)
+//	{
+//		printf("%d", a);
+//		a++;
+//	}
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	//char Pass[20] = {0};
+//	//char ret[20] = {0};	
+//	//int ch = 0;
+//	int Pass = 0;
+//	int ret = 0;
+//	printf ("password:");
+//	scanf("%d", Pass);
+//	/*while ((ch = getchar()) != '\n');*/
+//	printf("password:");
+//	scanf("%d", ret);
+//	if(Pass==ret)
+//	{ 
+//		printf("ok");
+//	}
+//	else
+//	{
+//		printf("sb");
+//	}	
+//	return 0;
+//}
+int main()
+{
+	//int ch = 0;
+	//char a[20];
+	char a = '1'; 
+	int ch = getchar();
+	if (a == ch)
+	{
+		printf("ok");
+	}
+	else
+	{
+		printf("sb");
+	}
+	
+	
+	
+	
+	
+	
+	/*putchar(ch);*/
+	/*if (a == ch) { printf("ok"); };*/
+
+
+
+	return 0;
+}
